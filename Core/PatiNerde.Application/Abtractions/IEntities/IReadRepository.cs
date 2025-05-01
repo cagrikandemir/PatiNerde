ad67ef52754dc7bdd57baf50ac9b20fa53d3e334
Çağrı <cagrikandemir58@icloud.com>
2025-05-02 00:53:04 +0300

    Read Repositoryler Yazıldı Interfaceleriyle birlikte

--- Core/PatiNerde.Application/Abtractions/IEntities/IReadRepository.cs
+++ Core/PatiNerde.Application/Abtractions/IEntities/IReadRepository.cs
@@ -1,5 +1,23 @@
-﻿namespace PatiNerde.Application.Abtractions.IEntities;
+﻿using Microsoft.EntityFrameworkCore;
+using PatiNerde.Domain.Entities;
+using System.Linq.Expressions;
 
-public interface IReadRepository
+namespace PatiNerde.Application.Abtractions.IEntities;
+
+public interface IReadRepository<T , TKey>
+    where T : BaseEntity
+    where TKey: IQueryable<TKey>
 {
+    DbSet<T> Table { get; }
+
+    DbContext Context { get; }
+
+    public IQueryable<T> GetAll();
+
+    public Task<T> FindByIdAsync(TKey Id);
+
+    public Task<ICollection<T>> FindListByIdAsync(TKey Id);
+
+    //public IQueryable<T?> Where(Expression<Func<T, bool>> method);
+
 }
--- External/PatiNerde.Persistence/Entities/ReadRepository.cs
+++ External/PatiNerde.Persistence/Entities/ReadRepository.cs
@@ -1,5 +1,52 @@
-﻿namespace PatiNerde.Persistence.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using PatiNerde.Application.Abtractions.IEntities;
+using PatiNerde.Domain.Entities;
+using PatiNerde.Persistence.Contexts;
 
-public class ReadRepository
+namespace PatiNerde.Persistence.Entities;
+
+public class ReadRepository<T, TKey> : IReadRepository<T, TKey>
+    where T : BaseEntity
+    where TKey : IQueryable<TKey>
 {
+    private readonly PatiNerdeDbContext _context;
+    public ReadRepository(PatiNerdeDbContext context)
+    {
+        _context = context;
+    }
+    public DbSet<T> Table => _context.Set<T>();
+
+    public DbContext Context => _context;
+
+    public async Task<T?> FindByIdAsync(TKey Id)
+    {
+        var entity = await Table.FindAsync(Id);
+        if (entity == null)
+            return null;
+        else
+            return entity;
+    }
+
+    public async Task<ICollection<T>> FindListByIdAsync(TKey Id)
+    {
+        ICollection<T> list = new List<T>();
+
+        T? entity = await FindByIdAsync(Id);
+
+        if (entity !=null)
+        {
+            list.Add(entity);
+        }
+        return await Task.FromResult(list);
+    }
+
+    public IQueryable<T> GetAll()
+    {
+        return Table.AsQueryable();
+    }
+
+    //public IQueryable<T?> Where(Expression<Func<T, bool>> method)
+    //{
+    //    throw new NotImplementedException();
+    //}
 }

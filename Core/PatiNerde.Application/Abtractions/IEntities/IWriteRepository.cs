bbcaddf001b006343a881c32bba39aa1e372735c
Çağrı <cagrikandemir58@icloud.com>
2025-05-04 14:20:34 +0300

    Write Repositoryler Yazıldı (Interface ile biirlikte)

--- Core/PatiNerde.Application/Abtractions/IEntities/IWriteRepository.cs
+++ Core/PatiNerde.Application/Abtractions/IEntities/IWriteRepository.cs
@@ -1,5 +1,30 @@
-﻿namespace PatiNerde.Application.Abtractions.IEntities;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
-public interface IWriteRepository
+namespace PatiNerde.Application.Abtractions.IEntities;
+
+public interface IWriteRepository<T, TKey>
+    where T : class
+    where TKey : IQueryable<TKey>
 {
+    DbSet<T> Table { get; }
+    DbContext Context { get; }
+
+    Task<T?> AddAsync(T model);
+
+    Task<ICollection<T>> AddMultiAsync(ICollection<T> models);
+
+    Task<bool> RemoveAsync(T model);
+
+    Task<bool> RemoveByIdAsync(TKey id);
+
+    Task<ICollection<T>> RemoveMultiAsync(ICollection<T> models);
+
+    Task<EntityEntry> UpdateAsync(T model);
+
+    Task<bool> UpdateByIdAsync(TKey id);
+
+    Task<ICollection<T>> UpdateMultiAsync(ICollection<T> models);
+
+    Task<int> SaveAsync(CancellationToken cancellationToken = default!);
 }
--- External/PatiNerde.Persistence/Entities/WriteRepository.cs
+++ External/PatiNerde.Persistence/Entities/WriteRepository.cs
@@ -1,5 +1,108 @@
-﻿namespace PatiNerde.Persistence.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using PatiNerde.Application.Abtractions.IEntities;
+using PatiNerde.Domain.Entities;
+using PatiNerde.Persistence.Contexts;
 
-public class WriteRepository
+namespace PatiNerde.Persistence.Entities;
+
+public class WriteRepository<T, TKey> : IWriteRepository<T, TKey>
+    where T : BaseEntity
+    where TKey : IQueryable<TKey>
 {
+    private readonly PatiNerdeDbContext _context;
+
+    public WriteRepository(PatiNerdeDbContext context)
+    {
+        _context = context;
+    }
+
+    public DbSet<T> Table => _context.Set<T>();
+
+    public DbContext Context => _context;
+
+    public async Task<T?> AddAsync(T model)
+    {
+        EntityEntry entity = await Table.AddAsync(model);
+        if (entity.State == await Task.FromResult(EntityState.Added))
+            return model;
+
+        return null;
+    }
+
+    public async Task<ICollection<T>> AddMultiAsync(ICollection<T> models)
+    {
+        if (models.Count <= 20)
+        {
+            foreach (T item in models)
+            {
+                EntityEntry entity = await Table.AddAsync(item);
+                if (entity.State != await Task.FromResult(EntityState.Added))
+                    models.Remove(item);
+            }
+            return models;
+        }
+        else
+            throw new InvalidOperationException("You can't add more than 20 items at once");
+    }
+
+    public async Task<bool> RemoveAsync(T model)
+    {
+        EntityEntry entity = Table.Remove(model);
+        return (entity.State == await Task.FromResult(EntityState.Deleted));
+    }
+
+    public async Task<bool> RemoveByIdAsync(TKey id)
+    {
+        T? model = await Table.FindAsync(id);
+        if (model == null)
+            return false;
+
+        return await RemoveAsync(model);
+    }
+
+    public async Task<ICollection<T>> RemoveMultiAsync(ICollection<T> models)
+    {
+        if (models.Count > 20)
+            throw new InvalidOperationException("You can't add more than 20 items at once");
+
+        foreach (T item in models)
+        {
+            if (!await RemoveAsync(item))
+                models.Remove(item);
+        }
+        return models;
+    }
+
+    public async Task<int> SaveAsync(CancellationToken cancellationToken = default)
+    {
+        return await _context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task<EntityEntry> UpdateAsync(T model)
+    {
+        EntityEntry entity = Table.Update(model);
+        return await Task.FromResult(entity);
+    }
+
+    public async Task<bool> UpdateByIdAsync(TKey id)
+    {
+        throw new Exception("Not implemented yet");
+        //T? model= await Table.FindAsync(id);
+        //if (model != null)
+        //    return await UpdateAsync(model);
+
+    }
+
+    public async Task<ICollection<T>> UpdateMultiAsync(ICollection<T> models)
+    {
+        if (models.Count >= 20)
+            throw new InvalidOperationException("You can't update more than 20 items at once");
+        foreach (T item in models)
+        {
+            if ((await UpdateAsync(item)).State != EntityState.Modified)
+                models.Remove(item);
+        }
+        return models;
+    }
 }

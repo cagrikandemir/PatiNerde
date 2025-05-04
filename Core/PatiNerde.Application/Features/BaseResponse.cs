480e309d8a722875e4d7035718c4b6b80cf5131b
Çağrı <cagrikandemir58@icloud.com>
2025-05-04 14:37:21 +0300

    Commands , Queryler ve Page Pagination İçin  Yapı Düzenlendi

---
+++ Core/PatiNerde.Application/Features/BaseResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace PatiNerde.Application.Features;
+
+public class BaseResponse
+{
+    public string? Code { get; set; }
+    public string? Title { get; set; }
+    public int TotalCount { get; set; } = 0;
+    public int Count { get; set; } = 0;
+}

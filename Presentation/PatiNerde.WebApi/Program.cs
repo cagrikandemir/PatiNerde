e0e302ede9d41cf43b2b34d215384e9a74dcdf2a
Çağrı <cagrikandemir58@icloud.com>
2025-05-05 11:22:38 +0300

    Projeye Swagger Eklentisi eklendi

--- Presentation/PatiNerde.WebApi/Program.cs
+++ Presentation/PatiNerde.WebApi/Program.cs
@@ -2,8 +2,16 @@
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddPersistenceServices();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(); // URL: /swagger
+}
 
 app.MapGet("/", () => "Hello World!");
 
